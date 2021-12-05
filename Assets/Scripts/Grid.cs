dcbd001d0aec55635ee6580a5718d9429c1258d6
anaego <anastas.ego@gmail.com>
2021-12-06 00:53:17 +0300

    Basic grid setup

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -5,23 +5,58 @@ using UnityEngine;
 
 public enum ElementType
 {
+    None,
     Fire,
     Water
 }
 
-[Serializable]
-public struct ElementPrefabType
-{
-    public ElementType Type;
-    public GameObject Element;
-}
-
 public class Grid : MonoBehaviour
 {
-    public int XDimension = 4;
-    public int YDimension = 6;
-    public ElementPrefabType[] ElementPrefabTypeMap;
+    public int Columns = 5;
+    public int Rows = 6;
+    public Element FireElementPrefab;
+    public Element WaterElementPrefab;
 
-    //private Dictionary<ElementType, GameObject> elementPrefabTypeMap;
+    private float xOffset = 0.5f;
+    private Dictionary<ElementType, Element> elementTypeMap;
+    private Element[,] allElements;
+    private ElementType[,] typeMap;
 
+    public void Start()
+    {
+        allElements = new Element[Columns, Rows];
+        elementTypeMap =  new Dictionary<ElementType, Element> {
+            { ElementType.Fire, FireElementPrefab },
+            { ElementType.Water, WaterElementPrefab }
+        };
+        // TODO load from a file
+        typeMap = new ElementType[5, 2] { 
+            { ElementType.Water, ElementType.Water }, 
+            { ElementType.None, ElementType.None }, 
+            { ElementType.Water, ElementType.Fire }, 
+            { ElementType.Fire, ElementType.None }, 
+            { ElementType.Fire, ElementType.None }
+        };
+        Setup();
+    }
+
+    private void Setup()
+    {
+        for (int i = 0; i <= Columns; i++)
+        {
+            if (i > typeMap.GetLength(0) - 1) break;
+            for (int j = 0; j <= Rows; j++)
+            {
+                if (j > typeMap.GetLength(1) - 1) break;
+                var elementType = typeMap[i, j];
+                if (elementType != ElementType.None)
+                {
+                    Instantiate(
+                        elementTypeMap[elementType], 
+                        new Vector2(i - (float)Columns / 2 + xOffset, j - (float)Rows / 2), 
+                        Quaternion.identity);
+                }
+            }
+        }
+    }
 }
